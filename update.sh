05a99781ce17ad3fae8664d9444bb20bd9369d4d
Mauricio Collazos <ma0@contraslash.com>
2019-03-02 12:09:52 -0500

    added configurations to use client certificate and client key

--- update.sh
+++ update.sh
@@ -20,7 +20,27 @@ if [ ! -z ${PLUGIN_KUBERNETES_CERT} ]; then
   KUBERNETES_CERT=${PLUGIN_KUBERNETES_CERT}
 fi
 
-kubectl config set-credentials default --token=${KUBERNETES_TOKEN}
+
+if [ ! -z ${PLUGIN_KUBERNETES_CLIENT_CERTIFICATE} ]; then
+  KUBERNETES_CLIENT_CERTIFICATE=$PLUGIN_KUBERNETES_CLIENT_CERTIFICATE
+fi
+
+if [ ! -z ${PLUGIN_KUBERNETES_CLIENT_KEY} ]; then
+  KUBERNETES_CLIENT_KEY=$PLUGIN_KUBERNETES_CLIENT_KEY
+fi
+
+if [ ! -z ${KUBERNETES_CLIENT_CERTIFICATE} ] && [ ! -z ${KUBERNETES_CLIENT_KEY} ]; then
+    echo ${KUBERNETES_CLIENT_CERTIFICATE} | base64 -d > client-certificate.crt
+    echo ${KUBERNETES_CLIENT_KEY} | base64 -d > client-key.crt
+    cat client-certificate.crt
+    echo "-----"
+    kubectl config set-credentials default --client-certificate client-certificate.crt --client-key client-key.crt
+else
+    kubectl config set-credentials default --token=${KUBERNETES_TOKEN}
+fi
+
+
+
 if [ ! -z ${KUBERNETES_CERT} ]; then
   echo ${KUBERNETES_CERT} | base64 -d > ca.crt
   kubectl config set-cluster default --server=${KUBERNETES_SERVER} --certificate-authority=ca.crt
@@ -32,7 +52,9 @@ fi
 kubectl config set-context default --cluster=default --user=${PLUGIN_KUBERNETES_USER}
 kubectl config use-context default
 
-# kubectl version
+#kubectl get nodes
+
+kubectl version
 IFS=',' read -r -a DEPLOYMENTS <<< "${PLUGIN_DEPLOYMENT}"
 IFS=',' read -r -a CONTAINERS <<< "${PLUGIN_CONTAINER}"
 for DEPLOY in ${DEPLOYMENTS[@]}; do
