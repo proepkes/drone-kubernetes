5c1d7fd48c98dc67b103ada68b08bbe6a21420c4
Phil <phil.boerner@gmx.de>
2019-07-05 16:12:38 +0200

    simplify update.sh

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM alpine:latest
-RUN apk --no-cache add curl ca-certificates bash
+RUN apk --no-cache add curl ca-certificates bash dos2unix
 RUN curl -Lo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 RUN chmod +x /usr/local/bin/kubectl
 COPY update.sh /bin/
+RUN dos2unix /bin/update.sh
+
 ENTRYPOINT ["/bin/bash"]
 CMD ["/bin/update.sh"]
--- update.sh
+++ update.sh
@@ -8,19 +8,6 @@ if [ -z ${PLUGIN_KUBERNETES_USER} ]; then
   PLUGIN_KUBERNETES_USER="default"
 fi
 
-if [ ! -z ${PLUGIN_KUBERNETES_TOKEN} ]; then
-  KUBERNETES_TOKEN=$PLUGIN_KUBERNETES_TOKEN
-fi
-
-if [ ! -z ${PLUGIN_KUBERNETES_SERVER} ]; then
-  KUBERNETES_SERVER=$PLUGIN_KUBERNETES_SERVER
-fi
-
-if [ ! -z ${PLUGIN_KUBERNETES_CERT} ]; then
-  KUBERNETES_CERT=${PLUGIN_KUBERNETES_CERT}
-fi
-
-
 if [ ! -z ${PLUGIN_KUBERNETES_CLIENT_CERTIFICATE} ]; then
   KUBERNETES_CLIENT_CERTIFICATE=$PLUGIN_KUBERNETES_CLIENT_CERTIFICATE
 fi
@@ -39,8 +26,6 @@ else
     kubectl config set-credentials default --token=${KUBERNETES_TOKEN}
 fi
 
-
-
 if [ ! -z ${KUBERNETES_CERT} ]; then
   echo ${KUBERNETES_CERT} | base64 -d > ca.crt
   kubectl config set-cluster default --server=${KUBERNETES_SERVER} --certificate-authority=ca.crt
@@ -52,19 +37,9 @@ fi
 kubectl config set-context default --cluster=default --user=${PLUGIN_KUBERNETES_USER}
 kubectl config use-context default
 
-kubectl get nodes
-
 kubectl version
 IFS=',' read -r -a DEPLOYMENTS <<< "${PLUGIN_DEPLOYMENT}"
-IFS=',' read -r -a CONTAINERS <<< "${PLUGIN_CONTAINER}"
 for DEPLOY in ${DEPLOYMENTS[@]}; do
-  echo Deploying to $KUBERNETES_SERVER
-  for CONTAINER in ${CONTAINERS[@]}; do
-    if [[ ${PLUGIN_FORCE} == "true" ]]; then
-      kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${DEPLOY} \
-        ${CONTAINER}=${PLUGIN_REPO}:${PLUGIN_TAG}FORCE
-    fi
-    kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${DEPLOY} \
-      ${CONTAINER}=${PLUGIN_REPO}:${PLUGIN_TAG} --record
-  done
+  echo Deploying ${DEPLOY} to $KUBERNETES_SERVER
+  kubectl -n ${PLUGIN_NAMESPACE} rollout restart deployment/${DEPLOY}
 done
